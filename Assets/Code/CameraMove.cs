97041d0408a6de39621df98fe680ea521b35c17a
kty424 <126883082+kty424@users.noreply.github.com>
2026-03-23 21:15:49 +0900

    add map
    stage1-map4
    stage1-map5
    stage1-map6

--- Assets/Code/CameraMove.cs
+++ Assets/Code/CameraMove.cs
@@ -14,10 +14,13 @@ public class CameraMove : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        Vector3 dir = new Vector3(offSetPosistion.x+ player_posistion.position.x - transform.position.x,
-           offSetPosistion.y + player_posistion.position.y - transform.position.y,0);
+        if (player_posistion != null)
+        {
+            Vector3 dir = new Vector3(offSetPosistion.x + player_posistion.position.x - transform.position.x,
+               offSetPosistion.y + player_posistion.position.y - transform.position.y, 0);
 
-        transform.position += dir;
+            transform.position += dir;
+        }
     }
 
 }
